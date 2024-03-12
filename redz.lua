e0fa34c6b2084104f23245c2a7cae5fe6d328050
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-12 21:28:47 +0700

    redz.lua

--- redz.lua
+++ redz.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/roblox_scripts/main/WelcomeUI.lua", true))()
