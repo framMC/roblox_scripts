72fe4854a6f8d34cc250eadfb7fdbe1231856b99
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-12 21:36:28 +0700

    Update WelcomeUI.lua

--- WelcomeUI.lua
+++ WelcomeUI.lua
@@ -1,1 +1,19 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
+Notification:Notify(
+    {Title = "LOG", Description = "ยินดีต้อนรับกลับ"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
+    {Image = "https://www.roblox.com/library/12017847837/logo-VernonX", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+)
+wait(1)
+Notification:Notify(
+    {Title = "LOG", Description = "เปิดใช้งานเพื่อลดการหน่วงของ Script"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 200, Type = "option"}
+)
+wait(1)
+Notification:Notify(
+    {Title = "LOG", Description = "Script โดย Team PWCSX"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 10, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
