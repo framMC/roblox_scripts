4aec4fd4a290d39c6354ad30bd29d8a9000b505a
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-14 22:13:58 +0700

    Create ambot-control.lua

---
+++ cheat/ambot-control.lua
@@ -0,0 +1,17 @@
+getgenv().keytoclick = "C"
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = keytoclick
+tool.Activated:connect(function()
+    local vim = game:service("VirtualInputManager")
+vim:SendKeyEvent(true, keytoclick, false, game)
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
+wait(0.2)
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+Notify({
+Description = "Loaded Key Successfully";
+Title = "Notification";
+Duration = 5;
+});
