3dd73aaefad952289b62c2561e8c7f66058422fc
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-14 22:16:13 +0700

    Update ambot-exe.lua

--- cheat/ambot-exe.lua
+++ cheat/ambot-exe.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/elxocasXD/Trip-Hub/main/Free%20Loader.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/framMC/roblox_scripts/main/cheat/ambot-control.lua'))()
