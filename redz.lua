aa02f473c6c4879b5e12dfeb99f91e45b51f4c33
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-30 00:00:30 +0700

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/roblox_scripts/main/WelcomeUI.lua", true))()
 
-game.Players.LocalPlayer:Kick("ขอโทษ สคริปต์ถูกปิดใช้งานเเล้ว หากต้องการเปิดให้ติดต่อทางเฟสบุค ขอบคุณ
-")
+game.Players.LocalPlayer:Kick("ขอโทษ สคริปต์ถูกปิดใช้งานเเล้ว หากต้องการเปิดให้ติดต่อทางเฟสบุค ขอบคุณ")
