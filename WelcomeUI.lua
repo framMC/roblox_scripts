161827751809d31632de975bbedbf3566e3b3e89
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-29 18:34:36 +0700

    Update WelcomeUI.lua

--- WelcomeUI.lua
+++ WelcomeUI.lua
@@ -2,24 +2,24 @@ local NotificationHolder = loadstring(game:HttpGet("https://raw.githubuserconten
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
 Notification:Notify(
-    {Title = "LOG", Description = "Welcome back!!"},
+    {Title = "บอท", Description = "ยินดีต้อนรับกลับ!!"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
     {Image = "https://www.roblox.com/library/12017847837/logo-VernonX", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
 )
 wait(1)
 Notification:Notify(
-    {Title = "LOG", Description = "Automatic activation By pressing the option below."},
+    {Title = "บอท", Description = "เปิดเจ้านเจ้านี้เพิ่อจำกัด ทรัพยากร ช่วยเพิ่ม เฟรมเรท"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 200, Type = "option"}
 )
 wait(1)
 Notification:Notify(
-    {Title = "LOG", Description = "Script Edit By pwcsx.fb"},
+    {Title = "บอท", Description = "สคริปต์โดย Moonlight เเละ Worachet!!"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 10, Type = "image"},
     {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 wait(1)
 Notification:Notify(
-    {Title = "LOG", Description = "Thanks For Run Scripting"},
+    {Title = "บอท", Description = "ขอให้สนุก"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
     {Image = "http://www.roblox.com/asset/?id=12017847837", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
