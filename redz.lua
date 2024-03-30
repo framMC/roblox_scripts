24d09d69fd82732ac613ab47428aa8c89b2441db
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-30 14:54:04 +0700

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,4 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/roblox_scripts/main/WelcomeUI.lua", true))()
-
-game.Players.LocalPlayer:Kick("คุณถูกเเบนออกจาก ประสบการณ์")
