2eb85369ce595ef565cdfdca714a9dd569c780c9
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-03-31 19:14:29 +0700

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,2 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/framMC/roblox_scripts/main/WelcomeUI.lua", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Swinsie/AFKSystem/master/LocalScript.lua'))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Swinsie/AFKSystem/master/FaderScript.lua", true))()
